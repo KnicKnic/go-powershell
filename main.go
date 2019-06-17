5e8dc5169213eefcf6329de36a84012cd799aa51
KnicKnic <knic.knic@gmail.com>
2019-06-16 17:22:06 -0700

    get cgo to compile with loader, wrong clr

--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
-import "fmt"
-
 /*
 
 #cgo CFLAGS: -IC:/code/go-net
 #cgo LDFLAGS: C:/code/go-net/mscoree.dll
+#include <stddef.h>
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <metahost.h>
-#pragma comment(lib, "mscoree.lib")
+// #include <metahost.h>
+// #pragma comment(lib, "mscoree.lib")
 
-ICLRMetaHost       *pMetaHost       = NULL;
-ICLRMetaHostPolicy *pMetaHostPolicy = NULL;
-ICLRDebugging      *pCLRDebugging   = NULL;
+// ICLRMetaHost       *pMetaHost       = NULL;
+// ICLRMetaHostPolicy *pMetaHostPolicy = NULL;
+// ICLRDebugging      *pCLRDebugging   = NULL;
 
 void myprint(char* s) {
-HRESULT hr = CLRCreateInstance(&CLSID_CLRMetaHost, &IID_ICLRMetaHost,
-                    (LPVOID*)&pMetaHost);
-	printf("%s\n", s);
+// HRESULT hr = CLRCreateInstance(&CLSID_CLRMetaHost, &IID_ICLRMetaHost,
+//                     (LPVOID*)&pMetaHost);
+
+    wchar_t* arr[] = {(wchar_t*)L"",(wchar_t*)L"c:\\someassembly.dll"};
+    int ret = oldmain(2,arr);
+	printf("\n%s, old_main %d\n", s,ret);
 
 
 }
@@ -37,5 +40,4 @@ func Example() {
 }
 func main() {
 	Example()
-	fmt.Println("hello world")
 }
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+int oldmain(int argc, wchar_t* argv[]);
+#ifdef __cplusplus
+}
+#endif
