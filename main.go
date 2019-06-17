49116c0930fefd995cd55d75652c0dbab6346256
KnicKnic <knic.knic@gmail.com>
2019-06-17 08:53:43 -0700

    add ability to pass data into powershell

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
+import (
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
 /*
 
-#cgo CFLAGS: -IC:/code/go-net
-#cgo LDFLAGS: C:/code/go-net/mscoree.dll
+#cgo CFLAGS: -IC:/code/psh_host
+#cgo LDFLAGS: c:/code/psh_host/x64/Release/psh_host.dll
 #include <stddef.h>
-#include "main.h"
+#include "host.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "sys/types.h"
 // #include <metahost.h>
 // #pragma comment(lib, "mscoree.lib")
 
@@ -15,13 +22,12 @@ package main
 // ICLRMetaHostPolicy *pMetaHostPolicy = NULL;
 // ICLRDebugging      *pCLRDebugging   = NULL;
 
-void myprint(char* s) {
+void myprint(void* unknown) {
+    wchar_t* s = (wchar_t*)unknown;
 // HRESULT hr = CLRCreateInstance(&CLSID_CLRMetaHost, &IID_ICLRMetaHost,
 //                     (LPVOID*)&pMetaHost);
-
-    wchar_t* arr[] = {(wchar_t*)L"",(wchar_t*)L"c:\\someassembly.dll"};
-    int ret = oldmain(2,arr);
-	printf("\n%s, old_main %d\n", s,ret);
+    long ret = startpowershell(s);
+	printf("\n%ws, old_main %d\n", s,ret);
 
 
 }
@@ -31,12 +37,14 @@ void myprint(char* s) {
 */
 import "C"
 
-import "unsafe"
-
 func Example() {
-	cs := C.CString("Hello from stdio\n")
-	C.myprint(cs)
-	C.free(unsafe.Pointer(cs))
+	// cs := C.CString("Hello from stdio\n")
+	cs, _ := windows.UTF16PtrFromString("c:\\fuzzy2")
+
+	ptrwchar := unsafe.Pointer(cs) // that is what you will get from Windows
+	// cs := C CString("Hello from stdio\n")
+	C.myprint(ptrwchar)
+	// C.free(unsafe.Pointer(cs))
 }
 func main() {
 	Example()
