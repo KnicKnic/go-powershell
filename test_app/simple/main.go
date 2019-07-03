8da227b3876f6a2450bde175d908f0505662daad
KnicKnic <knic.knic@gmail.com>
2019-07-03 09:10:10 -0700

    add simple example and update docs

---
+++ test_app/simple/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	powershell "github.com/KnicKnic/go-powershell"
+)
+
+func main() {
+	// create a runspace (where you run your powershell statements in)
+	runspace := powershell.CreateRunspaceSimple()
+	// auto cleanup your runspace
+	defer runspace.Delete()
+	
+	// execute a statement in powershell consisting of "emit this string"
+	// this will output that object into the results
+	results := runspace.ExecStr( `"emit this string"`, true)
+	// auto cleanup all results returned
+	defer results.Close()
+	
+	// print the string result of the first object
+	fmt.Println(results.Objects[0].ToString())
+	// Output: emit this string
+}
