8976c630d01fb445864a4005510db1f7249e607f
KnicKnic <knic.knic@gmail.com>
2019-07-05 00:04:09 -0700

    create a custom doc file

---
+++ pkg/powershell/doc.go
@@ -0,0 +1,6 @@
+/* 
+Package powershell allows hosting powershell sessions inside golang enabling bi directional communication between powershell and golang. 
+
+This package can call commandlets, scripts, keep session state between multiple invocations, allows for custom callback routines, and trapping host output redirecting to a custom logger. 
+*/
+package powershell
