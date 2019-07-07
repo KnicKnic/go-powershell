61cfa4df6ebe73475b2a7508d7512668270af8f7
KnicKnic <knic.knic@gmail.com>
2019-07-07 15:36:12 -0700

    Update readme example to show off more features

--- pkg/powershell/runspace_test.go
+++ pkg/powershell/runspace_test.go
@@ -29,23 +29,30 @@ func Example_savingVariablesAcrossStatements() {
 	// auto cleanup your runspace
 	defer runspace.Close()
 
-	statement1 := `$os = $env:OS;`
-	// The false says to not execute the statement in a seperate scope
-	// meaning that the variables will be availale to future invocations
-	results1 := runspace.ExecScript(statement1, false, nil)
+	// gets whatever environment variable we request
+	//     wrapping $args[0] inside $() so powershell understands [0] associated with $args
+	getEnvironmentVariable := `$environVariable = get-childitem "env:\$($args[0])";`
+
+	// Execute the statement
+	// false - says to not execute the statement in a temporary child scope
+	//     meaning that the variables will be available to future invocations
+	// nil - means we didn't name any arguments
+	// "OS" - after first 3 parameters comes the unnamed arguments which we reference via $args[index]
+	results1 := runspace.ExecScript(getEnvironmentVariable, false, nil, "OS")
 	//not defering close as we do not need the results
 	results1.Close()
 
-	statement2 := `"emitting your os is $os"`
-	// we are choosing the create in a different scope, the parent scope variables are accessible to us
-	// we could however choose to specify false and be in the same scope
-	results2 := runspace.ExecScript(statement2, true, nil)
+	returnEnvironmentInfo := `"emitting your $($environVariable.Name) is $($environVariable.Value)"`
+	// true - we are choosing the create in a temporary child scope, the parent scope variables are still accessible to us
+	//     we could however choose to specify false and be in the same scope
+	results2 := runspace.ExecScript(returnEnvironmentInfo, false, nil)
 	// auto cleanup all results returned
 	defer results2.Close()
 
+	// print the string result of the first object from the last statement (which happens to already be a string)
 	fmt.Println(results2.Objects[0].ToString())
 
-	// OUTPUT: emitting your os is Windows_NT
+	// Output: emitting your OS is Windows_NT
 }
 
 func Example_powershellCommand() {
--- readme.md
+++ readme.md
@@ -38,16 +38,31 @@ func main() {
 	runspace := powershell.CreateRunspaceSimple()
 	// auto cleanup your runspace
 	defer runspace.Close()
-	
-	// execute a statement in powershell consisting of "emit this string"
-	// this will output that object into the results
-	results := runspace.ExecScript( `"emit this string"`, true, nil)
+
+	// gets whatever environment variable we request
+	//     wrapping $args[0] inside $() so powershell understands [0] associated with $args
+	getEnvironmentVariable := `$environVariable = get-childitem "env:\$($args[0])";`
+
+	// Execute the statement
+	// false - says to not execute the statement in a temporary child scope
+	//     meaning that the variables will be available to future invocations
+	// nil - means we didn't name any arguments
+	// "OS" - after first 3 parameters comes the unnamed arguments which we reference via $args[index]
+	results1 := runspace.ExecScript(getEnvironmentVariable, false, nil, "OS")
+	//not deferring close as we do not need the results
+	results1.Close()
+
+	returnEnvironmentInfo := `"emitting your $($environVariable.Name) is $($environVariable.Value)"`
+	// true - we are choosing the create in a temporary child scope, the parent scope variables are still accessible to us
+	//     we could however choose to specify false and be in the same scope
+	results2 := runspace.ExecScript(returnEnvironmentInfo, false, nil)
 	// auto cleanup all results returned
-	defer results.Close()
-	
-	// print the string result of the first object
-	fmt.Println(results.Objects[0].ToString())
-	// Output: emit this string
+	defer results2.Close()
+
+	// print the string result of the first object from the last statement (which happens to already be a string)
+	fmt.Println(results2.Objects[0].ToString())
+
+	// Output: emitting your OS is Windows_NT
 }
 ```
 
